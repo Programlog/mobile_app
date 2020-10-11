f1f100f8bb06a3e154fb91751b03914593fee615
FlashCoder <anishwestwindsor@gmail.com>
2020-10-10 22:33:41 -0400

    adds user info to json. new signup screen success. Iteractive button to go back to login screen.

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
 
 Builder.load_file('design.kv')
+
 
 class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
 
+
 class SignUpScreen(Screen):
-    def add_user(self, uname, pname):
-        print(uname, pname)
+    def add_user(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+
+        users[uname] = {'username': uname, 'password': pword,
+                        'created': datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
+
+        self.manager.current = "sign_up_screen_success"
+
+
+class SignUpScreenSuccess(Screen):
+    def back_to_login(self):
+        self.manager.current = "login_screen"
+
 
 class RootWidget(ScreenManager):
     pass
+
 
 class MainApp(App):
     def build(self):
